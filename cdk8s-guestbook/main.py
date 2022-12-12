8262b39643955afc0d6050f554e8012306a8a33e
galbeniluz <gal498@gmail.com>
2022-12-12 20:10:37 +0200

    Update main.py

--- cdk8s-guestbook/main.py
+++ cdk8s-guestbook/main.py
@@ -4,32 +4,31 @@ from cdk8s import App, Chart
 
 from imports import k8s
 
-
 class MyChart(Chart):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
 
         label = {"app": "guestbook-ui"}
 
+
         k8s.KubeService(self, 'service',
             spec=k8s.ServiceSpec(
-              type='LoadBalancer',
-              ports=[k8s.ServicePort(port=80, target_port=k8s.IntOrString.from_number(80))],
-              selector=label))
+                type='LoadBalancer',
+                ports=[k8s.ServicePort(port=80, target_port=k8s.IntOrString.from_number(80))],
+                selector=label))
 
         k8s.KubeDeployment(self, 'deployment',
             spec=k8s.DeploymentSpec(
-                replicas=1,
+                replicas=2,
                 selector=k8s.LabelSelector(match_labels=label),
                 template=k8s.PodTemplateSpec(
-                  metadata=k8s.ObjectMeta(labels=label),
-                  spec=k8s.PodSpec(containers=[
-                      k8s.Container(
-                        name='guestbook-ui',
-                        image='gcr.io/heptio-images/ks-guestbook-demo:0.2',
-                        ports=[k8s.ContainerPort(container_port=80)])]))))
-
-
+                metadata=k8s.ObjectMeta(labels=label),
+                spec=k8s.PodSpec(containers=[
+                    k8s.Container(
+                    name='guestbook-ui',
+                    image='pgcr.io/heptio-images/ks-guestbook-demo:0.2',
+                    ports=[k8s.ContainerPort(container_port=80)])]))))
+        
 app = App()
 MyChart(app, "cdk8s-guestbook")
 
